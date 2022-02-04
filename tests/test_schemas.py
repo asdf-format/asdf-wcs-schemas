e40fd1d3897f4fb91ef62ccbc8c0b68db73295d0
William Jamieson <wjamieson@stsci.edu>
2022-02-04 10:53:35 -0500

    Fixed Windows

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -44,10 +44,16 @@ def test_required_properties(schema):
 
 
 def test_schema_style(schema_content):
-    assert schema_content.startswith(b"%YAML 1.1\n---\n")
-    assert schema_content.endswith(b"\n...\n")
+    # xor used because windows is sometimes weird.
+
+    assert schema_content.startswith(b"%YAML 1.1\n---\n") ^ schema_content.startswith(
+        b"%YAML 1.1\r\n---\r\n"
+    )
+    assert schema_content.endswith(b"\n...\n") ^ schema_content.endswith(b"\r\n...\r\n")
     assert b"\t" not in schema_content
-    assert not any(l != l.rstrip() for l in schema_content.split(b"\n"))
+    assert (not any(l != l.rstrip() for l in schema_content.split(b"\n"))) ^ (
+        not any(l != l.rstrip() for l in schema_content.split(b"\r\n"))
+    )
 
 
 def test_property_order(schema, manifest):
