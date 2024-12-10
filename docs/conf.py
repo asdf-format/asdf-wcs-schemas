81b82280ddf533b77c49aa584e01d47354e993fc
alphasentaurii <rkein@stsci.edu>
2024-12-10 10:26:50 -0500

    doc config settings for asdf-website subproject

--- docs/conf.py
+++ docs/conf.py
@@ -62,11 +62,24 @@ version = ".".join(release.split(".")[:2])
 # needs_sphinx = '1.2'
 
 intersphinx_mapping["pypa-packaging"] = ("https://packaging.python.org/en/latest/", None)
-intersphinx_mapping["asdf"] = ("https://asdf.readthedocs.io/en/latest/", None)
-intersphinx_mapping["asdf-standard"] = ("https://asdf-standard.readthedocs.io/en/latest/", None)
 intersphinx_mapping["asdf-astropy"] = ("https://asdf-astropy.readthedocs.io/en/latest/", None)
 intersphinx_mapping["pytest"] = ("https://docs.pytest.org/en/latest/", None)
 intersphinx_mapping["gwcs"] = ("https://gwcs.readthedocs.io/en/latest/", None)
+
+# Docs are hosted as a "subproject" under the main project's domain: https://www.asdf-format.org/projects
+# This requires including links to main project (asdf-website) and the other asdf subprojects
+# See https://docs.readthedocs.io/en/stable/guides/intersphinx.html#using-intersphinx
+subprojects = {
+    # main project
+    "asdf-website": ("https://www.asdf-format.org/en/latest", None),
+    # other subprojects
+    "asdf": ("https://asdf.readthedocs.io/en/latest/", None),
+    "asdf-coordinates-schemas": ("https://www.asdf-format.org/projects/asdf-coordinates-schemas/en/latest/", None),
+    "asdf-standard": ("https://asdf-standard.readthedocs.io/en/latest/", None),
+    "asdf-transform-schemas": ("https://www.asdf-format.org/projects/asdf-transform-schemas/en/latest/", None),
+}
+
+intersphinx_mapping.update(subprojects)  # noqa
 
 # To perform a Sphinx version check that needs to be more specific than
 # major.minor, call `check_sphinx_version("x.y.z")` here.
@@ -98,19 +111,31 @@ rst_epilog += """"""
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes. To override the custom theme, set this to the
 # name of a builtin theme or the name of a custom theme in html_theme_path.
-html_theme = "sphinx_rtd_theme"
-html_theme_options = {}
+html_theme = "furo"
+html_theme_options = {
+    "light_logo": "images/logo-light-mode.png",
+    "dark_logo": "images/logo-dark-mode.png",
+}
 
 html_static_path = ["_static"]
 
 # Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
+# Override default settings from sphinx_asdf / sphinx_astropy (incompatible with furo)
+html_sidebars = {}
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "_static/logo.ico"
-html_logo = "_static/logo.png"
+html_favicon = "_static/images/favicon.ico"
+html_logo = ""
+html_theme_options = {
+    "light_logo": "images/logo-light-mode.png",
+    "dark_logo": "images/logo-dark-mode.png",
+}
+
+pygments_style = "monokai"
+# NB Dark style pygments is furo-specific at this time
+pygments_dark_style = "monokai"
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -123,6 +148,18 @@ html_title = f"{project} v{release}"
 # Output file base name for HTML help builder.
 htmlhelp_basename = project + "doc"
 
+# Render inheritance diagrams in SVG
+graphviz_output_format = "svg"
+
+graphviz_dot_args = [
+    "-Nfontsize=10",
+    "-Nfontname=Helvetica Neue, Helvetica, Arial, sans-serif",
+    "-Efontsize=10",
+    "-Efontname=Helvetica Neue, Helvetica, Arial, sans-serif",
+    "-Gbgcolor=white",
+    "-Gfontsize=10",
+    "-Gfontname=Helvetica Neue, Helvetica, Arial, sans-serif",
+]
 
 # -- Options for LaTeX output --------------------------------------------------
 
@@ -130,7 +167,7 @@ htmlhelp_basename = project + "doc"
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [("index", project + ".tex", project + " Documentation", author, "manual")]
 
-latex_logo = "_static/logo.pdf"
+latex_logo = "_static/images/logo-light.png"
 
 
 # -- Options for manual page output --------------------------------------------
@@ -140,7 +177,7 @@ latex_logo = "_static/logo.pdf"
 man_pages = [("index", project.lower(), project + " Documentation", [author], 1)]
 
 sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname("__file__")), "sphinxext"))
-extensions += ["sphinx_asdf"]
+extensions += ["sphinx_asdf", "sphinx.ext.intersphinx", "sphinx.ext.extlinks"] # noqa
 
 
 def setup(app):
